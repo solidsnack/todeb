d3fcf782d9899fa3262e0d03ff59fe27aa4b7824
Jason Dusek <jason.dusek@gmail.com>
2010-07-23 14:37:15 -0700

    Added archiv handling.

---
+++ lib/todeb/archive.rb
@@ -0,0 +1,22 @@
+
+module ToDeb
+module Archive
+  TYPES = { '.tar.lzma' => %w| |,
+            '.tar.bz2' => %w| .tbz |,
+            '.tar.gz' => %w| .tgz | }
+extend self
+  def supported_archives_statement
+    TYPES.map do |canonical, variants|
+      [ canonical,
+        "(also #{variants.join(', ')})" unless variants.empty?
+      ].compact.join(' ')
+    end
+  end
+  def archive(files_map)
+    TYPES.select do |canonical, variants|
+      ([canonical] + variants).any?{|name| files_map.key? name }
+    end.first.first
+  end
+end
+end
+
