ba632cf4a306ab42d7f27e852368116f61d84e0b
Jason Dusek <jason.dusek@gmail.com>
2010-07-24 23:50:55 -0700

    Successful use of dh_make programmatically.

--- lib/todeb.rb
+++ lib/todeb.rb
@@ -4,9 +4,10 @@ require 'todeb/archive'
 module ToDeb
 
 WORKING = 'todeb-working-dir'
+MAINTAINER_REGEX = /^ *([^<]+) +<([^<>@]+@[^<>@]+)> *$/
 
 class BasicArchive
-  attr_reader :spec, :debian_name
+  attr_reader :spec, :debian_name, :dh_make_name
   def initialize(the_yaml)
     @spec = the_yaml
     @debian_name = "#{@spec['name']}_#{@spec['version']}"
@@ -18,9 +19,38 @@ class BasicArchive
       Archive.unpack_to_orig_dir(@spec['filesystem'], @dh_make_name)
     end
   end
+  def run_dh_make
+    begin
+      cmd = %w| dh_make --createorig --indep |
+      STDERR.puts "Running `dh_make'."
+      Dir.chdir("#{WORKING}/#{@dh_make_name}") do
+        env_set
+        system *cmd
+      end
+    ensure
+      env_unset
+    end
+  end
 private
   def archive_type
     Archive.type(@spec['filesystem'])
+  end
+  def env_set
+    @originals = %w| DEBFULLNAME DEBEMAIL PWD |.inject({}) do |acc, var|
+      acc[var] = ENV[var] if ENV[var]
+      acc
+    end
+    m = MAINTAINER_REGEX.match(@spec['maintainer'])
+    if m
+      ENV['DEBFULLNAME'] = m[1]
+      ENV['DEBEMAIL'] = m[2]
+    end
+    ENV['PWD'] = Dir.pwd
+  end
+  def env_unset
+    @originals.each do |var, val|
+      ENV[var] = val
+    end if @originals
   end
 end
 
